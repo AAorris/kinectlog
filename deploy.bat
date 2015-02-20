613f4cfbc5d09566ce6a096f2af66610bf9a0bed
Aaron Morris <aaorris@live.com>
2015-02-20 15:21:19 -0500

    Adding kinect work session

--- deploy.bat
+++ deploy.bat
@@ -7,4 +7,5 @@ xcopy worksessions %DeployLocation% /y /d
 set /p GitLocation=<git_location.txt
 echo Committing to %GitLocation%
 cd %GitLocation%
+git add .
 git commit -m "Adding kinect work session"
