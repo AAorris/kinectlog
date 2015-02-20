4aa921b93d12590b21fe535c21677e952a998313
Aaron Morris <aaorris@live.com>
2015-02-20 15:38:35 -0500

    Optional commit on deploy script

--- deploy.bat
+++ deploy.bat
@@ -1,11 +1,20 @@
-@ECHO on
+@ECHO off
 
+:deploy
 set /p DeployLocation=<deploy_location.txt
 echo Deploying to %DeployLocation%
 xcopy worksessions %DeployLocation% /y /d
 
+if not "%1" == "commit" goto nocommit
+
+:commit
 set /p GitLocation=<git_location.txt
 echo Committing to %GitLocation%
 cd %GitLocation%
-git add --all
-git commit -m "Adding kinect work session"
+git --git-dir=%GitLocation%/.git/ --work-tree=%GitLocation% add --all
+git --git-dir=%GitLocation%/.git/ --work-tree=%GitLocation% commit -m "Auto-Adding kinect work sessions"
+
+:nocommit
+echo not committing (call `deploy commit` if you want that)
+:end
+echo done.
