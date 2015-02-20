dc478e568009a903dc26ec5a5b15d23dfc42bc21
Aaron Morris <aaorris@live.com>
2015-02-20 15:10:40 -0500

    renaming
    processing doesn't like dashes

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,10 @@
 @ECHO off
+
 set /p DeployLocation=<deploy_location.txt
 echo Deploying to %DeployLocation%
-
 xcopy worksessions %DeployLocation% /y /d
+
+set /p GitLocation=<git_location.txt
+echo Committing to %GitLocation%
+cd %GitLocation%
+git commit -m "Adding kinect work session"
